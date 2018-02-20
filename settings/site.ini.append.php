e1a9dfcfd75dbb9b074c2c4813f0867245cf8989
Mark Howard <mark@clearfield.com>
2018-02-20 17:56:42 +1300

    Basic (untested) version of the Emarsys API with add contact call. Field mappings for the predefined Emarsys fields.

---
+++ settings/site.ini.append.php
@@ -0,0 +1,36 @@
+<?php /*
+
+[EmarsysAPI]
+URL=https://api.emarsys.net/api/v2/
+Username=philandteds002
+Secret=unknown
+
+
+FieldMappings=[]
+
+# user
+FieldMappings[first_name]=1
+FieldMappings[last_name]=2
+FieldMappings[email]=3
+
+# address
+FieldMappings[street_address]=10
+FieldMappings[city]=11
+FieldMappings[state]=12
+FieldMappings[zip]=13
+FieldMappings[country]=14
+FieldMappings[phone]=15
+FieldMappings[fax]=16
+
+# consumer profile
+
+#fields TBD
+#children_under_5
+#youngest_born_date
+#next_born_date
+#are_pregnant
+
+# this one is going to require some transformation
+#FieldMappings[email_subscription]=31
+
+*/ ?>
